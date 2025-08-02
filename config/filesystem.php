f3e489a2bb0e5341e0408ef1e125fc0707157102
zwass <zwass@yuanmeng.com>
2025-08-02 16:02:21 +0800

    更新七牛云地址

--- config/filesystem.php
+++ config/filesystem.php
@@ -23,12 +23,12 @@ return [
         // 更多的磁盘配置信息
         'qiniu' =>[									//完全可以自定义的名称
             'type'=>'qiniu',						//可以自定义,实际上是类名小写
-            'accessKey' =>'gagk0DqbsTzZlyxEA7pf46GUajqXqD0oiROotXOh',		//七牛云的配置,accessKey
-            'secretKey'=>'ymzPeW8BIs9eGBpx-2eMiqPwYBJ-PUhJOoqyCH-C',//七牛云的配置,secretKey
+            'accessKey' =>'_HPitLIk2EcSCHSpWEAXt8GO6kFGcYTg-yzS-lZh',		//七牛云的配置,accessKey
+            'secretKey'=>'A1GcUx65Z-2fbMtiTx56-X9s4QzEmYDzQxO4Z5Q0',//七牛云的配置,secretKey
             'bucket'=>'xinjh1215gsdigwi4564a',					//七牛云的配置,bucket空间名
             //'domain'=>'s2dgpwe6t.hn-bkt.clouddn.com'					//七牛云的配置,domain,域名
 //            'domain'=>'7niu.ybvgq.cn',				//七牛云的配置,domain,域名
-            'domain'=>'77niu.cerhc.cn',				//七牛云的配置,domain,域名
+            'domain'=>'cod.odzdu.cn',				//七牛云的配置,domain,域名
         ],
     ],
 ];
